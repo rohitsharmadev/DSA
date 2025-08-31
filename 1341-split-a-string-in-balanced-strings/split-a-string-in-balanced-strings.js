1d8e6503f5e61c8a8f1af7c7a9fb16b65416f39f
rohitsharmadev <55136106+rohitsharmadev@users.noreply.github.com>
2025-08-31 12:13:39 +0530

    Time: 1 ms (29.37%) | Memory: 55.8 MB (6.96%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+ //rohitsharmadev
+var balancedStringSplit = function(s) {
+   let temp = 0;
+   let count = 0;
+   for(let i= 0; i < s.length; i++) {
+    if(s[i]=== 'R') {
+        temp++;
+    } else {
+        temp--;
+    }
+    if(temp===0 ){
+        count++
+    }
+   } 
+   return count;
+};
